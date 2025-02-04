b5e1568a2021d879d7f4b26fe02a85f4378af32a
Ayush Upadhyay <58545547+ayushupadhyay358@users.noreply.github.com>
2025-02-04 21:17:19 +0530

    New changes

--- force-app/main/default/lwc/navContainParent/navContainParent.js
+++ force-app/main/default/lwc/navContainParent/navContainParent.js
@@ -1,18 +1,42 @@
 import { LightningElement, api, wire } from 'lwc';
-import getRecord from 'lightning/uiRecordApi';
+import {getRecord} from 'lightning/uiRecordApi';
+import getFieldData from '@salesforce/apex/navContainerHelper.getFieldData';
 
 export default class NavContainParent extends LightningElement {
 
     @api recordId;
     @api objectApiName;
 
-    @wire(getRecord,{ recordId: '$recordId', fields: ['Account.FirstName']})
+    objectFields = [];
+
+    constructor() {
+        super();
+    }
+
+
+    @wire(getRecord,{ recordId: '$recordId', fields: '$objectFields'})
     getRecordData({ data, error }){
         if (data) {
+            console.log("Record Data");
             console.log(JSON.stringify(data));
         }
         else if (error) {
             console.log(JSON.stringify(error));
         }
+    }
+
+    async connectedCallback() {
+        await this.fetchFieldData();
+    }
+
+    fetchFieldData() {
+        getFieldData({ ObjectName: this.objectApiName })
+            .then((data) => {
+                this.objectFields = data; // Populate objectFields
+                console.log('Field Data:', JSON.stringify(data));
+            })
+            .catch((error) => {
+                console.error('Error fetching field data:', JSON.stringify(error));
+            });
     }
 }
